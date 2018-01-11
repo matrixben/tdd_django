a35adfb47ba67ea042a91b7bbecc175f9ffd7509
matrixben <ben354164737@qq.com>
2018-01-11 14:48:07 +0800

    add some git ignore files

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,7 +1,42 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
 
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def testWhenOpenDjangoThenDisplayTODOListAPP(self):
+        # Jason听说有一个很酷的在线待办事项应用
+        # 他去看了这个应用的首页
+        self.browser.get('http://localhost:8000')
+
+        # 他注意到网页的标题和头部都包含"To-Do"这个词
+        self.assertIn('To-Do', self.browser.title)  # 判断前者是否后者的子字符串
+        self.fail('Finish the test!')  # 无论如何,使测试失败并返回其中的字符串
+
+
+# 应用邀请他输入一个待办事项
+
+# 他在一个文本框中输入"learn django in a month"
+
+# 他按回车键后,页面更新了
+# 待办事项表格中显示了"1.learn django in a month"
+
+# 页面中又显示了一个文本框,可以输入其他的待办事项
+# 他输入了"write a new virtual currency base on open source coin"
+
+# 页面再次更新,他的清单中显示了这两个待办事项
+
+# Jason想知道这个网站是否会记住他的清单
+
+# 他看到网站为他生成了一个惟一的url
+# 而且页面中有些文字解说这个功能
+
+# 他访问那个url,发现他的待办事项列表还在
+
+if __name__ == '__main__':
+    unittest.main()
