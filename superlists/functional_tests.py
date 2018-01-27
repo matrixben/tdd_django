d575f62ef22b9f7fea4b758523aea5649f1fdc17
matrixben <ben354164737@qq.com>
2018-01-27 11:01:04 +0800

    begin the to-do list project

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,18 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def testWhenOpenWebsiteThenShowTodolists(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+
+if __name__ == '__main__':
+    unittest.main()
